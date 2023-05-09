9c384bfcd01f73ca12a77ecee912522f134f1904
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2023-05-09 23:36:29 +0200

    basic database

--- src/main/java/nl/inholland/bankapi/services/TransactionService.java
+++ src/main/java/nl/inholland/bankapi/services/TransactionService.java
@@ -2,7 +2,9 @@ package nl.inholland.bankapi.services;
 
 import nl.inholland.bankapi.models.Transaction;
 import nl.inholland.bankapi.repositories.TransactionRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TransactionService {
     private TransactionRepository transactionRepository;
     public TransactionService(TransactionRepository transactionRepository) {
