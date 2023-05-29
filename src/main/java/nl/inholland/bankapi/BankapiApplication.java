5244fa194868db281170207a928e7aa88d213b8b
Salah-023 <90417315+Salah-023@users.noreply.github.com>
2023-05-29 17:09:04 +0200

    Cleaned some code
    removed some uncessary stuff and added pages to the getAllUsers for offset and limit

--- src/main/java/nl/inholland/bankapi/BankapiApplication.java
+++ src/main/java/nl/inholland/bankapi/BankapiApplication.java
@@ -9,32 +9,4 @@ public class BankapiApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankapiApplication.class, args);
     }
-
-//	@Bean
-//	public List<User> users() {
-//		return new ArrayList<>(List.of(
-//			new User("user1@email.com", "Dude", "+31000000000", 99.9, 99.9, User.Role.EMPLOYEE),
-//			new User("user2@email.com", "Lebowski ", "+31000000001", 99.9, 99.9, User.Role.CUSTOMER),
-//			new User("user3@email.com", "Maude ", "+31000000002", 99.9, 99.9, User.Role.CUSTOMER)
-//		));
-//	}
-//
-//	@Bean
-//	public List<BankAccount> bankAccounts() {
-//		return new ArrayList<>(List.of(
-//			new BankAccount( "NL55RABO6771015777", 100, 80.9,  BankAccount.AccountType.CURRENT),
-//			new BankAccount( "NL10RABO5262865534",  80.9, 1100.0, BankAccount.AccountType.SAVINGS),
-//			new BankAccount("NL55RABO6771015777",  80.9, 1000.0, BankAccount.AccountType.CURRENT),
-//			new BankAccount( "NL55RABO6771015777",  80.9, 1000.0, BankAccount.AccountType.SAVINGS),
-//			new BankAccount( "NL55RABO6771015777",  80.9, 1000.0, BankAccount.AccountType.CURRENT),
-//			new BankAccount( "NL55RABO6771015777", 80.9, 1000.0, BankAccount.AccountType.SAVINGS)
-//		));
-//	}
-//
-//	@Bean
-//	public List<Transaction> transactions() {
-//		return new ArrayList<>(List.of(
-//			new Transaction(1, "b5d6fd24-e379-11ed-b5ea-0242ac120002", LocalDate.of(2023, 1, 1), "NL55RABO6771015777", "NL10RABO5262865534", 100.0, "1cfc38d6-e378-11ed-b5ea-0242ac120002")
-//		));
-//	}
 }
--- src/main/java/nl/inholland/bankapi/repositories/UserRepository.java
+++ src/main/java/nl/inholland/bankapi/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package nl.inholland.bankapi.repositories;
 
 import nl.inholland.bankapi.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,5 +14,7 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByFirstNameAndLastName(String firstName, String lastName);
     Optional<User> findUserByEmail(String email);
+    Page <User> findAll(Pageable pageable);
+    Page<User> findAllByBankAccountsIsNull(Pageable pageable);
 
 }
